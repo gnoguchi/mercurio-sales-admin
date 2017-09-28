bb17f58cf913a79e9377cdfc97a64b873eaac70a
Diego <diegocoutinho@gmail.com>
2017-09-28 20:27:49 -0300

    melhorando cntrl login

--- controllers/login.js
+++ controllers/login.js
@@ -5,14 +5,17 @@ const passwordHash = require('password-hash');
 //realiza o login verificando senha em hash e setando um token.
 module.exports.login = (req, res) => {
   let query = { email: req.body.email };
-
-  UserSchema.findOne(query, (error, usuario) => {
-    if (usuario && passwordHash.verify(req.body.senha, usuario.senha)) {
-      const token = jwt.sign({ _id: usuario._id }, 'mercurio');
-      res.set('Authorization', token);
-      res.send(usuario, 200);
-      return;
-    };
-    res.sendStatus(400);
-  });
+  if(req.body.email == '' || req.body.senha == '') {
+    res.status(400).send('Preencha todos os campos.');
+  }else{
+    UserSchema.findOne(query, (error, usuario) => {
+      if (usuario && passwordHash.verify(req.body.senha, usuario.senha)) {
+        const token = jwt.sign({ _id: usuario._id }, 'mercurio');
+        res.set('Authorization', token);
+        res.send(usuario, 200);
+        return;
+      };
+      res.status(400).send('Email ou senha invalidos.');
+    });
+  };
 };
