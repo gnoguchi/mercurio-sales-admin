8fc99802059f79816a0c5fb5b5273a400e94e054
gabriel <gabriel1tdsc@gmail.com>
2017-09-29 17:30:24 -0300

    update analise negocios

--- controllers/analiseNegocios.js
+++ controllers/analiseNegocios.js
@@ -1,5 +1,6 @@
 const OperacaoSchema = require('../model/operacaoSchema')
 const ProdutoSchema = require('../model/produtoSchema')
+const calculos = require('../functions/calculoOperacoes')
 
 module.exports = {
 
@@ -65,40 +66,105 @@ module.exports = {
                         quantidadeVendido: vendas[i].quantidade
                     })
                     console.log(produtos);
-                    
+
                 })
             }
             res.send(resposta, 200)
         })
+    },
+
+    quantidadeVenda: (req, res) => {
+        OperacaoSchema.find({ tipo: 'venda' }, (err, vendas) => {
+            let qtVendas = 0;
+
+            if (err) {
+                return res.send(err)
+            }
+            for (resultVendas of vendas) {
+
+                qtVendas += resultVendas.quantidade
+            }
+            res.send(200, { qtTotal: qtVendas })
+
+        })
+    },
+
+    quantidadeCompra: (req, res) => {
+        OperacaoSchema.find({ tipo: 'compra' }, (err, compras) => {
+            let qtCompras = 0;
+
+            if (err) {
+                return res.send(err)
+            }
+            for (resultCompras of compras) {
+
+                qtCompras += resultCompras.quantidade
+            }
+            res.send(200, { qtTotal: qtCompras })
+
+        })
+    },
+
+    valorTotalCompra: (req, res) => {
+        OperacaoSchema.find({ tipo: 'compra' }, (err, compras) => {
+            let valorCompras = 0;
+            let valorProdutos = 0;
+
+            if (err) {
+                return res.send(err)
+            }
+            for (resultCompras of compras) {
+
+                ProdutoSchema.find((err, produtos) => {
+                    for (resultProdutos of produtos) {
+                        valorProdutos += resultProdutos.valor
+                    }
+                    valorCompras = calculos.calcularValorOperacao(valorProdutos, resultCompras.quantidade)
+                    res.send(200, { valorTotal: valorCompras })
+                })
+            }
+
+        })
+    },
+
+    valorTotalVenda: (req, res) => {
+        OperacaoSchema.find({ tipo: 'venda' }, (err, vendas) => {
+            let valorVendas = 0;
+            let valorProdutos = 0;
+            if (err) {
+                return res.send(err)
+            }
+            for (resultVendas of vendas) {
+
+                ProdutoSchema.find((err, produtos) => {
+                    for (resultProdutos of produtos) {
+                        valorProdutos += resultProdutos.valor
+                    }
+                    valorVendas = calculos.calcularValorOperacao(valorProdutos, resultVendas.quantidade)
+                    res.send(200, { valorTotal: valorVendas })
+                })
+            }
+
+        })
+    },
+
+    listaVendas: (req, res) => {
+        OperacaoSchema.find({ tipo: 'venda' }, (err, vendas) => {
+            if (err) {
+                return res.send(err)
+            }
+
+            res.send(vendas, 200)
+        })
+    },
+
+    listaCompras: (req, res) => {
+        OperacaoSchema.find({ tipo: 'compra' }, (err, compras) => {
+            if (err) {
+                return res.send(err)
+            }
+
+            res.send(compras, 200)
+        })
     }
-    // consultarMaisVendidos: (req, res) => {
-    //     console.log('bateu');
-    //     OperacaoSchema.find({ tipo: 'venda' }).sort('-quantidade')
-    //         .limit(5).exec((err, resultado) => {
-    //             if (err) {
-    //                 return res.send(err)
-    //             }
-    //             let resposta = []
-    //             for (maisVendido in resultado) {
-
-    //                 ProdutoSchema.findById(maisVendido.produto, (err, produto) => {
-    //                     if (err) {
-    //                         return res.send(err)
-    //                     }
-
-    //                     this.resposta.push({
-    //                         nome: produto.nome,
-    //                         descricao: produto.descricao,
-    //                         valorProduto: produto.valor,
-    //                         quantidadeVendido: maisVendido.quantidade
-    //                     })
-
-    //                 })
-    //             }
-
-    //             res.send(resposta)
-
-
-    //         })
-    // }
 }
